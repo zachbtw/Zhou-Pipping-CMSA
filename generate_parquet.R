5a78d91a2f40720880224c8744f7cccc670560e1
Lou-Zhou <louzhou6@gmail.com>
2025-06-20 15:06:22 -0400

    data wrangling start

---
+++ generate_parquet.R
@@ -0,0 +1,15 @@
+library(arrow)
+library(tidyverse)
+# tracking <- read_csv("nfl-big-data-bowl-2025/tracking_week_1.csv") |> 
+#    bind_rows(read_csv("nfl-big-data-bowl-2025/tracking_week_2.csv")) |> 
+#    bind_rows(read_csv("nfl-big-data-bowl-2025/tracking_week_3.csv")) |> 
+#    bind_rows(read_csv("nfl-big-data-bowl-2025/tracking_week_4.csv")) |> 
+#    bind_rows(read_csv("nfl-big-data-bowl-2025/tracking_week_5.csv")) |> 
+#    bind_rows(read_csv("nfl-big-data-bowl-2025/tracking_week_6.csv")) |> 
+#    bind_rows(read_csv("nfl-big-data-bowl-2025/tracking_week_7.csv")) |> 
+#    bind_rows(read_csv("nfl-big-data-bowl-2025/tracking_week_8.csv")) |> 
+#    bind_rows(read_csv("nfl-big-data-bowl-2025/tracking_week_9.csv"))
+#  arrow::write_parquet(tracking, "nfl-big-data-bowl-2025/tracking.parquet")
+ 
+ arrow::read_parquet("nfl-big-data-bowl-2025/tracking.parquet")
+ 
